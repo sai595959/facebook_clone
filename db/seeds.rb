d1638f77588c038ed517e26935da4ee5e9c94aa0
simon <sai590817@gmail.com>
2017-07-12 19:16:03 +0900

    seeds;2C

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,13 @@
-10.times do |n|
-  name = Faker::LordOfTheRings.character
-  email = Faker::Internet.email
-  password = Faker::Internet.password
-  uid = SecureRandom.uuid
-  User.create!(
-              id: n,
-              name: name,
-              email: email,
-               password: password,
-               password_confirmation: password,
-               uid: uid
-               )
+20.times do |n|
+name = Faker::LordOfTheRings.character
+email = Faker::Internet.email
+password = Faker::Internet.password
+uid = SecureRandom.uuid
+User.create(
+id: n,
+name: name,
+email: email,
+password: password,
+uid: uid
+)
 end
