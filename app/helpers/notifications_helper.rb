9ab1a51861f700fdcc998226ceb847dd2ba927ff
simon <sai590817@gmail.com>
2017-07-14 17:14:50 +0900

    comment_notification

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -2,4 +2,12 @@ module NotificationsHelper
   def posted_time(time)
     time > Date.today ? "#{time_ago_in_words(time)}" : time.strftime('%m月%d日')
   end
+
+  def notification_read(notification)
+    if notification.read?
+      "既読"
+    else
+      "未読"
+    end
+  end
 end
